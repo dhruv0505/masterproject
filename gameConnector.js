079a3927ea1fc1be6117723a91e243822fff545d
dhruv0505 <77700089+dhruv0505@users.noreply.github.com>
2021-01-22 17:52:03 -0800

    Carousel + Collapsible

--- gameConnector.js
+++ gameConnector.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, options);
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
 
 d3.select("#button1")
  .on("click",() => {
