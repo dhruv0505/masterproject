f183a346564801dcdaa5e4c0a4c1fa779939f3dd
dhruv0505 <77700089+dhruv0505@users.noreply.github.com>
2021-01-28 11:39:47 -0800

    Tooltips Home, Game 1-3

--- gameConnector.js
+++ gameConnector.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, options);
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, options);
+});
 
 d3.select("#button1")
  .on("click",() => {
